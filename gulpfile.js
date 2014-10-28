4a7529188e262be709f4d8eaa25eb9d9a3cdc409
John Otander <johnotander@gmail.com>
2014-10-28 15:39:37 -0600

    Add html example that links the generated markdown and css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,35 @@
-var gulp      = require('gulp'),
-    name      = require('gulp-rename'),
-    rework    = require('gulp-rework'),
-    reworkNPM = require('rework-npm'),
-    md        = require('gulp-remarkable');
+var gulp        = require('gulp'),
+    name        = require('gulp-rename'),
+    rework      = require('gulp-rework'),
+    reworkNPM   = require('rework-npm'),
+    includer    = require('gulp-htmlincluder'),
+    classPrefix = require('rework-class-prefix'),
+    md          = require('gulp-remarkable');
 
 gulp.task('md', function() {
   return gulp.src('air.md')
     .pipe(md())
-    .pipe(name('index.html'))
+    .pipe(name('-markdown.html'))
+    .pipe(gulp.dest('example'));
+});
+
+gulp.task('html', ['md', 'css'], function() {
+  return gulp.src('example/*.html')
+    .pipe(includer())
     .pipe(gulp.dest('./'));
 });
 
 gulp.task('css', function() {
   return gulp.src('index.css')
-    .pipe(rework(reworkNPM()))
+    .pipe(rework(reworkNPM(), classPrefix('air-')))
     .pipe(name('air.css'))
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['md', 'css']);
+gulp.task('watch', function() {
+  gulp.watch(['*.md', '*.css'], function() {
+    gulp.start('default');
+  });
+});
+
+gulp.task('default', ['md', 'css', 'html', 'watch']);
